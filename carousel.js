3211d96bfe55586cac3ef48bb0cb80521158af33
MariEdler <113932184+MariEdler@users.noreply.github.com>
2024-04-16 21:40:39 -0700

    Add files via upload

--- carousel.js
+++ carousel.js
@@ -1,24 +1,23 @@
 
 let currentSlide = 0;
 const slides = document.querySelectorAll('.carouselImages a');
-const totalSlides = slides.length / 1; // Since we show 3 images at a time
+const totalSlides = slides.length / 1;
 
 function showSlide(index) {
-    // Calculate the margin-left for the carousel
-    const marginLeft = index * -400; // 400px width * 3 images
-    document.querySelector('.carouselImages').style.marginLeft = `${marginLeft}px`;
+  
+    const marginLeft = index * -400;
+    document.querySelector('.carouselImages').style.marginLeft = marginLeft + 'px';
 }
 
 function moveSlide(direction) {
     currentSlide += direction;
     if (currentSlide < 0) {
-        currentSlide = 0; // Prevent going before the first item
+        currentSlide = 0;
         
     } else if (currentSlide +2 >= totalSlides) {
-        currentSlide = totalSlides - 6; // Prevent going past the last item
+        currentSlide = totalSlides - 6;
     }
     showSlide(currentSlide);
 }
 
-// Initialize the carousel with the first group of images displayed
 showSlide(currentSlide);
